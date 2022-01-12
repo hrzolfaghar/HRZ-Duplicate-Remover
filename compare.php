51ff67db62bdc106ab4a3ffece0fc4de8f02b27a
hrzolfaghar <97587005+hrzolfaghar@users.noreply.github.com>
2022-01-12 11:00:45 +0330

    Add files via upload

---
+++ compare.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @package HRZ Duplicate Refiner
+ * @author Hamidreza Zolfaghar hrz.zolfaghar@gmail.com
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or later
+ */
+
+
+/** input directories :
+ *  $patternDir: The directory used as the pattern & does not change
+ *  $victimDir: A directory that is compared to the pattern directory, Then files that have the same name and the same content are removed from this directory. Finally, empty directories also will be removed.
+ */
+$patternDir = "";
+$victimDir = "";
+
+
+define('DS', DIRECTORY_SEPARATOR);
+
+class Compare
+{
+    private string $patternDir;
+    private string $victimDir;
+    private string $basePath;
+    private array $filesList = [];
+
+    function __construct($patternDir, $victimDir)
+    {
+        $this->patternDir = $patternDir;
+        $this->victimDir = $victimDir;
+        $this->basePath = getcwd();
+        echo "Refining similar items in the \"$this->victimDir\" Directory compared to \"$this->patternDir\" Directory ...";
+        echo '<br/><br/>';
+        $this->filesList($this->basePath . DS . $this->victimDir);
+        $this->Refiner();
+    }
+
+    function filesList($check)
+    {
+        # $removePath is the first part of the path that we do not want to be stored in the $filesList array.
+        $removePath = $this->basePath . DS . $this->victimDir . DS;
+
+        $items = scandir($check);
+
+        foreach ($items as $item) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            } elseif (is_file($check . DS . $item)) {
+                $this->filesList[] = str_replace($removePath, '', $check . DS . $item);
+            } elseif (is_dir($check . DS . $item)) {
+                $this->filesList($check . DS . $item);
+            } else {
+                exit('Unexpected Error: please contact HRZ.ZOLFAGHAR@GMAIL.COM');
+            }
+        }
+    }
+
+    function Refiner()
+    {
+        $patternPath = $this->basePath . DS . $this->patternDir;
+        $victimPath = $this->basePath . DS . $this->victimDir;
+
+        foreach ($this->filesList as $file) {
+
+            if (file_exists($patternPath . DS . $file)
+                && filesize($patternPath . DS . $file) == filesize($victimPath . DS . $file)) {
+                $fileDirPath = dirname($victimPath . DS . $file);
+                unlink($victimPath . DS . $file);
+                echo "File \"" . $victimPath . DS . $file . "\" is similar & removed.";
+                echo '<br/>';
+
+                if (count(scandir($fileDirPath)) == 2) {
+                    rmdir($fileDirPath);
+                    echo "Directory \"" . $fileDirPath . "\" has become an empty directory & removed.";
+                    echo '<br/>';
+                }
+            }
+        }
+    }
+
+    function __destruct()
+    {
+        echo '<br/>';
+        echo "Refining completed successfully.";
+    }
+}
+
+new Compare('en', 'fa');
